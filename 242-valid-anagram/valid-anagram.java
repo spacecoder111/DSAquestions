eebac4c9e5e785200c9a071b8ca707635c15ff1c
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-07-10 08:58:55 +0530

    Time: 2 ms (99.61%) | Memory: 44.8 MB (55.07%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        if(n!=m) return false;
+        int arr1[]=new int[26];
+        int arr2[]=new int[26];
+        for(char ch:s.toCharArray()){
+            arr1[ch-'a']++;
+        }
+         for(char ch:t.toCharArray()){
+            arr2[ch-'a']++;
+        }
+         for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
+    }
+}
