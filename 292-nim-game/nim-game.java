64170cceeefd52da4beeb1c3f8c121aca115427c
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-07-11 16:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.04%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean canWinNim(int n) {
-        boolean b=true;
-        while(n%4==0){
-            n-=3;
-            b=!b;
-        }
-        return b;
+        // boolean b=true;
+        // while(n%4==0){
+        //     n-=3;
+        //     b=!b;
+        // }
+        return n%4!=0;
     }
 }
