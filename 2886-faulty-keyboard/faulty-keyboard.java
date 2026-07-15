c3e28eefe720b7ce4622a2118807778a897fb69a
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-07-15 23:19:05 +0530

    Time: 3 ms (98.24%) | Memory: 46.6 MB (31.41%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(ch=='i'){
+                sb.reverse();
+                continue;
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
