24a9d8524df0b95d154f1dc285749229ef961286
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-07-31 17:23:40 +0530

    Time: 38 ms (15.32%) | Memory: 47.9 MB (87.86%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minimumPushes(String word) {
+        Map<Character,Integer> map=new HashMap<>();
+        Set<Character> set=new HashSet<>();
+        int arr[]=new int[26];
+        for(char ch:word.toCharArray()){
+            arr[ch-'a']++;
+        } 
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<26;i++){
+            int idx=-1;
+            int max=-1;
+            for(int j=0;j<26;j++){
+                if(arr[j]==0) continue;
+               if(arr[j]>max){
+                max=arr[j];
+                idx=j;
+               }
+            }
+            if(idx==-1) break;
+            arr[idx]=0;
+            sb.append((char)('a'+idx));
+        }
+        int c=0;
+        for(char ch:sb.toString().toCharArray()){
+            if(set.contains(ch)) continue;
+            if(c<=7){
+                map.put(ch,1);
+            }else if(c<=15){
+                map.put(ch,2);
+            }else if(c<=23){
+                map.put(ch,3);
+            }else{
+                map.put(ch,4);
+            }
+            c++;
+            set.add(ch);
+        }
+        int ans=0;
+        for(char ch:word.toCharArray()){
+            ans+=map.get(ch);
+        }
+        return ans;
+    }
+}
