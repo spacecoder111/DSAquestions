e01f4e978a224948061f911543cdaf75ffb1511d
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-07-31 17:25:52 +0530

    Time: 9 ms (94.22%) | Memory: 47.8 MB (94.51%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -6,6 +6,8 @@ class Solution {
         for(char ch:word.toCharArray()){
             arr[ch-'a']++;
         } 
+        int c=0;
+        int ans=0;
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<26;i++){
             int idx=-1;
@@ -19,27 +21,37 @@ class Solution {
             }
             if(idx==-1) break;
             arr[idx]=0;
-            sb.append((char)('a'+idx));
-        }
-        int c=0;
-        for(char ch:sb.toString().toCharArray()){
-            if(set.contains(ch)) continue;
             if(c<=7){
-                map.put(ch,1);
+                ans+=max;
             }else if(c<=15){
-                map.put(ch,2);
+                 ans+=max*2;
             }else if(c<=23){
-                map.put(ch,3);
+                 ans+=max*3;
             }else{
-                map.put(ch,4);
+                 ans+=max*4;
             }
             c++;
-            set.add(ch);
+           // sb.append((char)('a'+idx));
         }
-        int ans=0;
-        for(char ch:word.toCharArray()){
-            ans+=map.get(ch);
-        }
+        
+        // for(char ch:sb.toString().toCharArray()){
+        //     if(set.contains(ch)) continue;
+        //     if(c<=7){
+        //         map.put(ch,1);
+        //     }else if(c<=15){
+        //         map.put(ch,2);
+        //     }else if(c<=23){
+        //         map.put(ch,3);
+        //     }else{
+        //         map.put(ch,4);
+        //     }
+        //     c++;
+        //     set.add(ch);
+        // }
+        
+        // for(char ch:word.toCharArray()){
+        //     ans+=map.get(ch);
+        // }
         return ans;
     }
 }
