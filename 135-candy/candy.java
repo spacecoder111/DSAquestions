a5c64bee321522463baf3472098bf0ee48145b0e
Akash74 <149812862+spacecoder111@users.noreply.github.com>
2026-08-03 22:30:00 +0530

    Time: 3 ms (86.44%) | Memory: 47.9 MB (6.08%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,22 +1,48 @@
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int left[]=new int[n];
-        int right[]=new int[n];
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i-1]<ratings[i]) left[i]=left[i-1]+1;
-            else left[i]=1;
-        }
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i+1]<ratings[i]) right[i]=right[i+1]+1;
-            else right[i]=1;
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=Math.max(left[i],right[i]);
+        int i=1;
+        int ans=n;
+        while(i<n){
+            if(ratings[i-1]==ratings[i]){
+                //ans+=1;
+                i++;
+                continue;
+            }
+            int c=0;
+            while(i<n && ratings[i-1]<ratings[i]){
+                c++;
+                ans+=c;
+                i++;
+            }
+            int d=0;
+            while(i<n && ratings[i-1]>ratings[i]){
+                d++;
+                ans+=d;
+                i++;
+            }
+            //if(c<d){
+                ans-=Math.min(d,c);
+           // }
         }
         return ans;
     }
 }
+
+// int left[]=new int[n];
+//         int right[]=new int[n];
+//         left[0]=1;
+//         for(int i=1;i<n;i++){
+//             if(ratings[i-1]<ratings[i]) left[i]=left[i-1]+1;
+//             else left[i]=1;
+//         }
+//         right[n-1]=1;
+//         for(int i=n-2;i>=0;i--){
+//             if(ratings[i+1]<ratings[i]) right[i]=right[i+1]+1;
+//             else right[i]=1;
+//         }
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             ans+=Math.max(left[i],right[i]);
+//         }
+//         return ans;
